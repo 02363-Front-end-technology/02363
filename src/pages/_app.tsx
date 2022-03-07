89edc999a1f255ad631766b157e693bca0484555
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2022-03-07 16:47:55 +0100

    Added supabase react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from 'react-supabase'
+import { supabase } from '@Utils/supabaseClient'
+
 
 const MyApp = ({ Component, pageProps }) => {
   return (
+    <Provider value={supabase}>
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
+  </Provider>
+
   )
 }
 
