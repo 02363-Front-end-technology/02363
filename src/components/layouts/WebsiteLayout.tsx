ec99dfad0accec837549a3caa7fd665eba762b9d
Loui Nissen-Petersen <59645750+ItzLue@users.noreply.github.com>
2022-03-29 21:14:05 +0200

    Update WebsiteLayout.tsx

--- src/components/layouts/WebsiteLayout.tsx
+++ src/components/layouts/WebsiteLayout.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { IProduct, IUpgradeItem } from '@Interfaces/index';
+import { IProduct } from '@Interfaces/index';
 import NavBar from '@Components/layouts/NavBar';
 import Add from './Add';
 import style from '@Styles/WebsiteLayout.module.css';
 import TopGameBar from '@Components/topGameBar/TopGameBar';
 import { useRecoilValue } from 'recoil';
 import { currentUserGameData } from '../../atoms';
-import ProductCard from '@Components/Products/ProductCard';
 import ProductList from '@Components/Products/ProductList';
 
 const UpgradeLayout = () => {
 	const frontendItems = useRecoilValue(currentUserGameData).items[0].upgrades;
+	const adds = useRecoilValue(currentUserGameData).items[2].upgrades;
 	return (
 		<div style={{ height: '100%' }}>
 			<TopGameBar />
 			<div className={style.layout}>
 				{frontendItems[0].isBought && <NavBar />}
 				<div className={style.container}>
-					<Add src='/AddblockAdd.png' />
-					<Add src='/InvisibleAd.png' />
+					{adds[0].isBought && <Add src='/AddblockAdd.png' />}
+					{adds[1].isBought && <Add src='/AddblockAdd.png' />}
 				</div>
-					<ProductList products={products} />
+				<ProductList products={products} />
 			</div>
 		</div>
 	);
