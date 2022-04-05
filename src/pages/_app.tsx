1faedcf30dded5f5306d5d1ede2970af3674dabc
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2022-04-05 14:50:21 +0200

    Add react hot toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,12 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-supabase';
 import { supabase } from '@Utils/supabaseClient';
 import { RecoilRoot } from 'recoil';
+import { Toaster } from 'react-hot-toast';
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
 		<RecoilRoot>
+			<div><Toaster/></div>
 			<Provider value={supabase}>
 				<ChakraProvider>
 					<Component {...pageProps} />
