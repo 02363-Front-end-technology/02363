d2382ff713ce47bb0d7da8d6549f638200a9f9ea
Loui Nissen-Petersen <59645750+ItzLue@users.noreply.github.com>
2022-04-11 07:30:34 +0200

    jest

--- __tests__/game.text.jsx
+++ __tests__/game.text.jsx
@@ -1,16 +1,16 @@
 import { screen } from '@testing-library/react';
 import { getPage } from 'next-page-tester';
 
-describe('Game page', () => {
-	it('should render the game page', async () => {
+describe('New game page', () => {
+	it('should render the new game page', async () => {
 		const { render } = await getPage({
 			route: '/',
 			dotenvFile: '.env.local',
 		});
 		render();
 
-	//	const ahref = screen.getByRole('button', { name: 'New Game' });
-		// expect(ahref).toBeInTheDocument();
+		//const ahref = screen.getByRole('button', { name: 'New Game' });
+	//	expect(ahref).toBeInTheDocument();
 	});
 
 });
