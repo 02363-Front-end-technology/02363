6e5e0d046aa0a28f53782dd6a92541ce628e43d4
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2022-04-25 01:29:11 +0200

    style changes to product cards (More clean)

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -8,12 +8,15 @@ type IProps = {
 
 const ProductCard: React.FC<IProps> = ({ product }) => {
 	return (
-		<div className='box-border h-full max-w-max overflow-hidden rounded-tl-lg rounded-br-lg border-2 border-red-100 shadow-lg'>
-			<Image className='rounded-tl-lg rounded-br-lg' alt='Product' src={product.image} layout='fixed' objectFit='cover' width={250} height={175} />
+		<div className='group box-border h-full max-w-max overflow-hidden rounded-tl-lg rounded-br-lg border-2 border-red-100 shadow-lg shadow-red-100
+		transition duration-150 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:shadow-lg hover:border-red-200 
+		hover:cursor-pointer		
+		'>
+			<Image className='rounded-tl-lg rounded-br-lg transition duration-150 ease-in-out  group-hover:brightness-125' alt='Product' src={product.image} layout='fixed' objectFit='cover' width={250*1.5} height={175*1.5} />
 			<div className='p-2'>
-				<p className='text-orange-700'>{product.description}</p>
-				<p className='text-green-500'>{product.price}$</p>
-				<p className='font-bold text-amber-500'>{product.rating}★</p>
+				<p className='text-orange-700 transition duration-150 ease-in-out  group-hover:animate-pulse group-hover:text-red-600'>{product.description}</p>
+				<p className='text-green-500 transition duration-150 ease-in-out  group-hover:animate-pulse group-hover:text-red-600'>{product.price}$</p>
+				<p className='font-bold text-amber-500 transition duration-150 ease-in-out  group-hover:animate-pulse group-hover:text-red-600'>{product.rating}★</p>
 			</div>
 		</div>
 	);
