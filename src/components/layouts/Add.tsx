922f06eb358bcc2d41597ee2430e29fc0682f0cd
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2022-04-25 01:16:11 +0200

    ads now look a lot nicer. Made them more clean

--- src/components/layouts/Add.tsx
+++ src/components/layouts/Add.tsx
@@ -2,7 +2,19 @@ import Image from 'next/image';
 
 type IProps = {
 	src: string;
+	title: string;
+	width: number;
+	height: number;
 };
-const Add: React.FC<IProps> = ({ src }) => <Image alt='Add' src={src} layout='fixed' width={300} height={200} />;
+function Ad({ src, title }) {
+	return (
+		<div className='relative hover:cursor-pointer hover:opacity-50 transition duration-150'>
+			<Image className='brightness-50 ' alt={"Ad"} src={src} layout='fill' objectFit='cover' />
+			<div className='relative w-full'>
+				<h2 className='pb-8 pt-14 text-center text-4xl font-semibold text-slate-200'>{title}</h2>
+			</div>
+		</div>
+	);
+}
 
-export default Add;
+export default Ad;
--- src/components/layouts/WebsiteLayout.tsx
+++ src/components/layouts/WebsiteLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from '@Components/layouts/NavBar';
-import Add from './Add';
+import Ad from './Add';
 import style from '@Styles/WebsiteLayout.module.css';
 import TopGameBar from '@Components/topGameBar/TopGameBar';
 import { useRecoilValue } from 'recoil';
@@ -17,11 +17,21 @@ const UpgradeLayout = () => {
 			<TopGameBar />
 			<div className={style.layout}>
 				{frontendItems[1].isBought && <NavBar />}
-				<div className={style.container}>
-					{adds[0].isBought && <Add src='/AddblockAdd.png' />}
-					{adds[1].isBought && <Add src='/AddblockAdd.png' />}
-				</div>
+				<span className='flex max-h-96  w-full bg-slate-800 '>
+					<div className={'w-full '}>
+						{
+							// Photo by Abd Elrahman Elokby from Pexels: https://www.pexels.com/photo/stylish-female-standing-with-glass-of-soda-on-street-6779997/
+							adds[0].isBought && <Ad title={'GET YOUR CLOTHES AT OSAS'} src='/ad_2.jpg' />
+						}
+					</div>
+				</span>
 				{frontendItems[0].isBought && <ProductList products={products} />}
+				<div className={'w-full '}>
+					{
+						// Photo by Engin Akyurt: https://www.pexels.com/photo/a-delicious-burger-on-paper-placemat-5374421/
+						adds[1].isBought && <Ad title={"Frankie's Steak House"} src='/ad_3.jpg' />
+					}
+				</div>
 			</div>
 		</div>
 	);
