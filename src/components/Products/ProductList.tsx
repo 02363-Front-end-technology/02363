d3ad7cdce17f481f63999efa8a049692d1f2bb2c
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2022-04-25 01:32:13 +0200

    product list responsiveness

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -8,7 +8,7 @@ type IProps = {
 
 const ProductList: React.FC<IProps> = ({ products }) => {
 	return (
-		<div className='my-12 grid w-full grid-cols-1 justify-items-center gap-4 sm:grid-cols-3'>
+		<div className='my-12 grid w-full grid-cols-1 justify-items-center gap-4 lg:grid-cols-2 xl:grid-cols-3'>
 			{products.map((p, index) => (
 				<ProductCard key={index} product={p} />
 			))}
